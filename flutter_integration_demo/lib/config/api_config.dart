049ef0f4117e70aff6ba68c2fd91773466831bca
rendramh <rendramhardika@gmail.com>
2025-12-03 00:13:33 +0700

    remove credential from flutter integration project

--- flutter_integration_demo/lib/config/api_config.dart
+++ flutter_integration_demo/lib/config/api_config.dart
@@ -4,18 +4,15 @@
 class ApiConfig {
   // OpenWeatherMap API Key
   // Get your free API key from: https://openweathermap.org/api
-  // static const String openWeatherMapApiKey = 'PASTE_YOUR_OPENWEATHERMAP_API_KEY_HERE';
-  static const String openWeatherMapApiKey = '587bd8dc2f4641b7365d23008d4f8725';
+  static const String openWeatherMapApiKey = 'PASTE_YOUR_OPENWEATHERMAP_API_KEY_HERE';
   
   // WAQI (World Air Quality Index) API Key
   // Get your free API key from: https://aqicn.org/data-platform/token/
-  // static const String waqiApiKey = 'PASTE_YOUR_WAQI_API_KEY_HERE';
-  static const String waqiApiKey = '6b7fa7ffb03735797cf08b3a98d1900da54f281b';
+  static const String waqiApiKey = 'PASTE_YOUR_WAQI_API_KEY_HERE';
   
   // NewsAPI.org API Key
   // Get your free API key from: https://newsapi.org/register
-  // static const String newsApiKey = 'PASTE_YOUR_NEWSAPI_API_KEY_HERE';
-    static const String newsApiKey = '4fc2e823d4e640f7b0281e8dfec9a525';
+  static const String newsApiKey = 'PASTE_YOUR_NEWSAPI_API_KEY_HERE';
   
   // API Base URLs
   static const String openWeatherMapBaseUrl = 'https://api.openweathermap.org/data/2.5';
